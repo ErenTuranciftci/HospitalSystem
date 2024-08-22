b4586f7c549c772583398c926103fd6c37ddd3b7
ErenTuranciftci <erenturanciftci@gmail.com>
2024-08-22 20:09:55 +0300

    Doctormainform yarıtıldı

---
+++ HospitalSystem.WinFormUI/DoctorMainForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace HospitalSystem.WinFormUI
+{
+    partial class DoctorMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDepartman = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstDoctorAppointments = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblDepartman
+            // 
+            this.lblDepartman.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblDepartman.Location = new System.Drawing.Point(171, 304);
+            this.lblDepartman.Name = "lblDepartman";
+            this.lblDepartman.Size = new System.Drawing.Size(100, 23);
+            this.lblDepartman.TabIndex = 76;
+            this.lblDepartman.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblPhone.Location = new System.Drawing.Point(171, 271);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(100, 23);
+            this.lblPhone.TabIndex = 75;
+            this.lblPhone.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblEmail.Location = new System.Drawing.Point(171, 236);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(100, 23);
+            this.lblEmail.TabIndex = 74;
+            this.lblEmail.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblSurname.Location = new System.Drawing.Point(171, 200);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(100, 23);
+            this.lblSurname.TabIndex = 73;
+            this.lblSurname.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblName
+            // 
+            this.lblName.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblName.Location = new System.Drawing.Point(171, 164);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(100, 23);
+            this.lblName.TabIndex = 72;
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblUserName.Location = new System.Drawing.Point(171, 131);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(100, 23);
+            this.lblUserName.TabIndex = 70;
+            this.lblUserName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(65, 304);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 23);
+            this.label8.TabIndex = 69;
+            this.label8.Text = "Department";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(65, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 65;
+            this.label4.Text = "Surname";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(65, 271);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 66;
+            this.label5.Text = "Phone";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(65, 236);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 23);
+            this.label6.TabIndex = 67;
+            this.label6.Text = "Email";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(65, 164);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 23);
+            this.label7.TabIndex = 68;
+            this.label7.Text = "Name";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(65, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 64;
+            this.label3.Text = "UserName";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(463, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 23);
+            this.label1.TabIndex = 62;
+            this.label1.Text = "Doctor Appointments";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lstDoctorAppointments
+            // 
+            this.lstDoctorAppointments.FormattingEnabled = true;
+            this.lstDoctorAppointments.Location = new System.Drawing.Point(457, 103);
+            this.lstDoctorAppointments.Name = "lstDoctorAppointments";
+            this.lstDoctorAppointments.Size = new System.Drawing.Size(243, 290);
+            this.lstDoctorAppointments.TabIndex = 61;
+            // 
+            // DoctorMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDepartman);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstDoctorAppointments);
+            this.Name = "DoctorMainForm";
+            this.Text = "DoctorMainForm";
+            this.Load += new System.EventHandler(this.DoctorMainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDepartman;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstDoctorAppointments;
+    }
+}
---
+++ HospitalSystem.WinFormUI/DoctorMainForm.cs
@@ -0,0 +1,47 @@
+﻿using HospitalSystem.BLL.DesignPatterns.GenericRepository.ConcReps;
+using HospitalSystem.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HospitalSystem.WinFormUI
+{
+    public partial class DoctorMainForm : Form
+    {
+        Doctor _authDoctor;
+        AppointmentRepository _appRep;
+
+        public DoctorMainForm(Doctor doctor)
+        {
+            InitializeComponent();
+            _authDoctor = doctor;
+            _appRep = new AppointmentRepository();
+        }
+
+        private void DoctorMainForm_Load(object sender, EventArgs e)
+        {
+            lblUserName.Text = _authDoctor.UserName;
+            lblName.Text = _authDoctor.DoctorProfile.Name;
+            lblSurname.Text = _authDoctor.DoctorProfile.Surname;
+            lblEmail.Text = _authDoctor.DoctorProfile.Email;
+            lblPhone.Text = _authDoctor.DoctorProfile.Phone;
+            lblDepartman.Text = _authDoctor.DoctorProfile.Department.ToString();
+
+            foreach (Appointment item in _authDoctor.Appointments)
+            {
+                if(item.Patient != null && item.StartDate > DateTime.Now)
+                {
+                    lstDoctorAppointments.Items.Add(item);
+                }
+            }
+
+            
+        }
+    }
+}
